0bb10f0e5502f03e7a75efc3e62f04c227558c3f
Will Hoffman <will.hoffman@pm.me>
2026-07-04 20:17:44 -0500

    move site images, add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hoffmn.com',
-    output: 'server'
+  site: 'https://hoffmn.com',
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
